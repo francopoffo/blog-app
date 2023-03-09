7310d7f518f9968abc8a602da0846df73b612f4c
Franco Poffo <franco_sutter@yahoo.com.br>
2023-03-09 14:55:28 -0300

    feat: Terminando de configurar a página específica do post com react-markdown

--- src/components/posts/post-detail/post-content.tsx
+++ src/components/posts/post-detail/post-content.tsx
@@ -1,18 +1,57 @@
 import { Post } from "@/types/post";
 import React from "react";
+import Image from "next/image";
 import ReactMarkdown from "react-markdown";
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 const PostContent: React.FC<{ post: Post }> = (props) => {
   const { post } = props;
 
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
+  const customRenderers = {
+    p(paragraph: any) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+
+    code(code: any) {
+      const { className, children } = code;
+      const language = className.split("-")[1];
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          // eslint-disable-next-line react/no-children-prop
+          children={children}
+        />
+      );
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 };
